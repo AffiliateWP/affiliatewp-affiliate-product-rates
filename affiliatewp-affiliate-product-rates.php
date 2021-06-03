160dc758d3c168e8e48fa29e3ec11bb6d01972af
dbeja <david.beja@gmail.com>
2021-06-03 17:57:21 +0100

    Bump the AffiliateWP minimum to 2.6. #40

--- affiliatewp-affiliate-product-rates.php
+++ affiliatewp-affiliate-product-rates.php
@@ -54,6 +54,24 @@ class AffiliateWP_APR_Requirements_Check extends AffiliateWP_Requirements_Check 
 	protected $slug = 'affiliatewp-affiliate-product-rates';
 
 	/**
+	 * Add-on requirements.
+	 *
+	 * @since 1.0.0
+	 * @var   array[]
+	 */
+	protected $addon_requirements = array(
+		// AffiliateWP.
+		'affwp' => array(
+			'minimum' => '2.6',
+			'name'    => 'AffiliateWP',
+			'exists'  => true,
+			'current' => false,
+			'checked' => false,
+			'met'     => false,
+		),
+	);
+
+	/**
 	 * Bootstrap everything.
 	 *
 	 * @since 1.1
