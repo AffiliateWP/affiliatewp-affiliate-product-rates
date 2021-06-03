5edb9bd482eef6dde6218874a31074ede03a63b9
dbeja <david.beja@gmail.com>
2021-06-03 17:11:44 +0100

    Recreate main plugin file. #41

---
+++ affiliatewp-affiliate-product-rates.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Affiliate Product Rates
+ * Plugin URI: https://affiliatewp.com/add-ons/official-free/affiliate-product-rates/
+ * Description: Set per-affiliate product referral rates
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.0.5.1
+ * Text Domain: affiliatewp-affiliate-product-rates
+ * Domain Path: languages
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Affiliate Product Rates
+ * @category Core
+ * @author Andrew Munro
+ * @version 1.0.5.1
+ */
