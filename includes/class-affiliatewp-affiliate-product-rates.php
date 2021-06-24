edbc3a03c6716f6096ecd6529ad888ee7954613c
David Beja <david.beja@gmail.com>
2021-06-24 10:29:17 +0100

    issue/19 - Allow language translations to be handled by WP repo (#37)
    Fixes #19

--- includes/class-affiliatewp-affiliate-product-rates.php
+++ includes/class-affiliatewp-affiliate-product-rates.php
@@ -68,7 +68,6 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Product_Rates' ) ) {
 				self::$instance = new AffiliateWP_Affiliate_Product_Rates;
 				self::$instance->file = $file;
 				self::$instance->setup_constants();
-				self::$instance->load_textdomain();
 				self::$instance->hooks();
 				self::$instance->includes();
 
@@ -182,39 +181,6 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Product_Rates' ) ) {
 			if ( is_admin() ) {
 				require_once AFFWP_APR_PLUGIN_DIR . 'includes/class-admin.php';
 				require_once AFFWP_APR_PLUGIN_DIR . 'includes/scripts.php';
-			}
-		}
-
-
-		/**
-		 * Loads the plugin language files
-		 *
-		 * @access public
-		 * @since 1.0
-		 * @return void
-		 */
-		public function load_textdomain() {
-			// Set filter for plugin's languages directory.
-			$lang_dir = dirname( plugin_basename( AFFWP_APR_PLUGIN_DIR ) ) . '/languages/';
-			$lang_dir = apply_filters( 'affwp_affiliate_product_rates_languages_directory', $lang_dir );
-
-			// Traditional WordPress plugin locale filter.
-			$locale        = apply_filters( 'plugin_locale',  get_locale(), 'affiliatewp-affiliate-product-rates' );
-			$mofile        = sprintf( '%1$s-%2$s.mo', 'affiliatewp-affiliate-product-rates', $locale );
-
-			// Setup paths to current locale file.
-			$mofile_local  = $lang_dir . $mofile;
-			$mofile_global = WP_LANG_DIR . '/affiliatewp-affiliate-product-rates/' . $mofile;
-
-			if ( file_exists( $mofile_global ) ) {
-				// Look in global /wp-content/languages/affiliatewp-affiliate-product-rates folder.
-				load_textdomain( 'affiliatewp-affiliate-product-rates', $mofile_global );
-			} elseif ( file_exists( $mofile_local ) ) {
-				// Look in local /wp-content/plugins/affiliatewp-affiliate-product-rates/languages/ folder.
-				load_textdomain( 'affiliatewp-affiliate-product-rates', $mofile_local );
-			} else {
-				// Load the default language files.
-				load_plugin_textdomain( 'affiliatewp-affiliate-product-rates', false, $lang_dir );
 			}
 		}
 
@@ -455,5 +421,4 @@ if ( ! class_exists( 'AffiliateWP_Affiliate_Product_Rates' ) ) {
 	function affiliatewp_affiliate_product_rates() {
 		return AffiliateWP_Affiliate_Product_Rates::instance();
 	}
-
 }
